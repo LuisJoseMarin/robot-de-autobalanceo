c9dde76e5ba5feba439865606d45c3fa98a89ecf
Luis José Marín <32722325+LuisJoseMarin@users.noreply.github.com>
2017-11-07 20:12:10 -0600

    Prueba de motores con acelerometro
    Se unió el ejemplo del acelerometro y el de los motores en un programa que incluya los dos factores

---
+++ Ejemplos/Prueba_de_motores_con_acelerometro/Prueba_de_motores_con_acelerometro.ino
@@ -0,0 +1,44 @@
+const int rightf = 11;
+const int rightb = 10;
+const int leftf = 9;
+const int leftb = 8;               
+const int zpin = A2; 
+String out = "";
+
+void setup() {
+  pinMode(rightf, OUTPUT);
+  pinMode(rightb, OUTPUT);
+  pinMode(leftf, OUTPUT);
+  pinMode(leftb, OUTPUT);
+  Serial.begin(9600);
+
+}
+
+void loop() {
+ int z = analogRead(zpin);
+ float zero_G = 338.0;
+ float zero_Gz=340.0;
+ float scale_z = 68;
+
+ Serial.println(out);
+
+ if (((float)z - zero_Gz)/scale_z >= 0.10){
+    digitalWrite(rightf, HIGH);
+    digitalWrite(rightb, LOW);
+    digitalWrite(leftf, HIGH);
+    digitalWrite(leftb, LOW);
+    out = "Forward";   
+ } else if (((float)z - zero_Gz)/scale_z <= -0.10){
+    digitalWrite(rightf, LOW);
+    digitalWrite(rightb, HIGH);
+    digitalWrite(leftf, LOW);
+    digitalWrite(leftb, HIGH); 
+    out = "Backward";
+ }else{
+    digitalWrite(rightf, LOW);
+    digitalWrite(rightb, LOW);
+    digitalWrite(leftf, LOW);
+    digitalWrite(leftb, LOW);
+    out = "Center";
+ }
+}
